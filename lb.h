ed05ae848b8e6c173a39ee0a00474a14899d69f1
wang18678665352-lgtm <wang18678665352@126.com>
2026-04-17 22:30:12 +0800

    可以从文件里加载

--- lb.h
+++ lb.h
@@ -6,6 +6,12 @@ typedef struct node{
         char vlaue[MAX_VLAUE];
         struct node *next;
 }Node;
-Node *create_node(char *vlaue);
+
+Node *create_node_head(char *vlaue, Node *head);
+Node *create_node_tail(char *vlaue, Node *head);
+Node *load_data_from_file(const char *filename, Node *head);
 void free_node(Node*p);
-void print(Node*head);              
+
+void print(const char *filename, Node*head);              
+
+
--- lianbiao.c
+++ lianbiao.c
@@ -1,31 +1,93 @@
 #include "lb.h"
+#include <locale.h>
+#include <unistd.h>
+
 
 Node *head =NULL;
-Node *create_node(char *vlaue){
+Node *create_node_head(char *vlaue, Node *head){
      Node*p = (Node*)malloc(sizeof(Node));
      strcpy(p->vlaue, vlaue);
      p->next = head;
      head = p;
      return p;
-};
-void free_node(Node*p){
-     while(p){     
-        p = p->next;
-        free(p);
-        };
-};
-void print(Node*head){
-     while(head){
-        printf("%s",head->vlaue);
+}
+Node *create_node_tail(char *vlaue, Node *head){
+     Node*p = (Node*)malloc(sizeof(Node));
+     strcpy(p->vlaue, vlaue);
+     p->next = NULL;
+     if (head == NULL) {
+         head = p;
+     } else {
+         Node* tmp = head;
+         while (tmp->next != NULL) {
+             tmp = tmp->next;
+         }
+         tmp->next = p;
+     }
+     return head;
+}
+
+void free_node(Node* head){
+    Node* tmp;
+    while(head != NULL){
+        tmp = head;      // 先保存当前节点
+        head = head->next; // 跳到下一个节点
+        free(tmp);       // 再释放
+    }
+}
+Node *load_data_from_file(const char *filename, Node *head) {
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error: Cannot open file %s\n", filename);
+        return head;
+    }
+    char buffer[MAX_VLAUE];
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        buffer[strcspn(buffer, "\n")] = 0; // remove newline
+        head = create_node_tail(buffer, head);
+    }
+    fclose(fp);
+    return head;
+}
+
+void print(const char *filename, Node*head){
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL) {
+        printf("Error: Cannot open file %s\n", filename);
+        return;
+    }
+    while(head){
+        // printf("%s",head->vlaue);
+        fprintf(fp,"%s\n",head->vlaue);
         head = head->next;
-    };
-};
+    }
+    fclose(fp);
+}
+
+
 int main(){
-    char a[MAX_VLAUE];
-    scanf("%s",a);
-    create_node(a);
-    print(head);
-    free_node(head);
-    return 0;
+    Node *head =load_data_from_file("data/input.txt", NULL);
+    // setlocale(LC_ALL, "chs");  // 设置为中文 locale
+    printf("[Success] File opened successfully\n");
     
-};
+    printf("Please enter a string: ");
+    fflush(stdout);  // Immediately flush output
+
+    char a[MAX_VLAUE];
+    do {
+        scanf("%s", a);
+        if (strcmp(a, "0") != 0) {
+            head = create_node_tail(a, head);
+        }
+    } while (strcmp(a, "0") != 0);
+    print("data/test.txt", head);
+    free_node(head);
+    printf("[Success] Memory freed\n");
+    
+    printf("[Success] File closed\n");
+    printf("[Success] Program finished! Data saved to data/test.txt\n");
+    
+    system("pause");  // Pause reliably
+    
+    return 0;
+}
